f49fd54870396d17eaeb64e7fb7dfb1ab9eb3bd4
Stefan Eder <50300987+MasterEda92@users.noreply.github.com>
2024-06-13 07:32:14 +0200

    deleted unnecessary duplicate file

--- MediatorLib/AddCommandHandler.cpp
+++
@@ -1,8 +0,0 @@
-#include "AddCommandHandler.h"
-#include "AddCommand.h"
-
-std::unique_ptr<ICommandResult> CAddCommandHandler::Handle(const ICommand& command)
-{
-   int nResult = ((CAddCommand&)command).GetFirst() + ((CAddCommand&)command).GetSecond();
-   return std::make_unique<CAddCommandResult>(nResult);
-}
